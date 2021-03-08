72a707a87fb815ef81e721da40f95fd1e7cc6784
pwo1735 <srg198@gmail.com>
2021-03-08 17:46:00 +0300

    added blog-one

---
+++ app/images/products/big/Metod_setok_granichnye_uslovia_pervogo_roda_na_kosinuse.pas
@@ -0,0 +1,77 @@
+﻿uses crt;
+
+var p,q,a,b,a0,b0,g0,a1,b1,g1,h: real;
+    x,ap,bp,cp,d,Pp,Qp,y: array [0..100] of real;
+    N,i: integer;
+  
+begin
+  clrscr;
+  writeln('Введите коэффициенты p и q (q<0):');
+  write('p(x)= ');
+  readln(p);
+  write('q(x)= ');
+  readln(q);
+  writeln('Введите концы отрезка:');
+  write('a= ');
+  readln(a);
+  write('b= ');
+  readln(b);
+  writeln('Введите коэффициенты граничных условий:');
+  write('a0= ');
+  readln(a0);
+  write('b0= ');
+  read(b0);
+  write('g0= ');
+  readln(g0);
+  write('a1= ');
+  readln(a1);
+  write('b1= ');
+  readln(b1);
+  write('g1= ');
+  readln(g1);
+  writeln('Введите количество узлов');
+  write('N= ');
+  readln(N);
+  x[0]:=a;
+  h:=(b-a)/N;
+  for i:=1 to N do
+    x[i]:=x[i-1]+h;
+  
+  
+  //задаем матрицу со первой до строки N:
+  for i:=1 to N-1 do
+  begin
+    ap[i]:=(2-p*h)/(2*h*h);
+    bp[i]:=q-2/(h*h);
+    cp[i]:=(p*h+2)/(2*h*h);
+    d[i]:=-cos(x[i])-3*sin(x[i])-3*cos(x[i]); //столбец свободных коэффициентов
+  end;
+  
+  //зададим первую и последнюю строки:
+  bp[0]:= a0-b0/h;
+  cp[0]:=b0/h;
+  d[0]:=g0;
+  bp[N]:=a1-b1/h;
+  ap[N]:=b1/h;
+  d[N]:=g1;
+  
+  //решим систему методом прогонки
+  Pp[0] := -cp[0]/bp[0];
+  Qp[0] := d[0]/bp[0];
+  FOR i := 1 TO n - 1  DO
+    BEGIN
+      Pp[i]:= -cp[i]/(bp[i]+ap[i]*Pp[i-1]);
+      Qp[i]:= (d[i] - ap[i]*Qp[i-1])/(bp[i]+ap[i]*Pp[i-1]);
+    END;
+  y[n] :=(d[n] - ap[n]*Qp[n-1])/(bp[n]+ap[n]*Pp[n-1]);
+  FOR i := n-1 DOWNTO 0 DO
+    y[i] := Pp[i]*y[i+1]+ Qp[i];
+  writeln('Узел:                          Решение:              Ожидается:         Погрешность:');
+  for i:=0 to N do
+  begin
+    writeln('x[',i,']=',x[i]:20:16,'y[',i,']=',y[i]:20:16,cos(x[i]):20:16,abs(y[i]-cos(x[i])):20:16);
+  end;
+  
+  readkey;
+  
+end.
--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,81 @@
 $(function () {
+
+  $('.product-menu__item').on('click', function () {
+    $(this).toggleClass('product-menu__item--active');
+  });
+
+
+
+  $('.catalog-list__button').on('click', function () {
+    $(this).toggleClass('catalog-list__button--active');
+
+  });
+
+  $('.product-tabs__top-item').on('click', function(e) {
+    e.preventDefault();
+    $('.product-tabs__top-item').removeClass('product-tabs__top-item--active');
+    $(this).addClass('product-tabs__top-item--active');
+
+    $('.product-tabs__content-item').removeClass('product-tabs__content-item--active');
+    $($(this).attr('href')).addClass('product-tabs__content-item--active');
+  });
+
+  $('.product-one__num').styler();
+
+
+
+  $('.filter-price__input').ionRangeSlider({
+    type: "double",
+    prefix: "$",
+    onStart: function (data) {
+      prefix: "$";
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+    onChange: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+    
+  });
+  $(".filter-recent__rate").rateYo({
+    readOnly: true,
+    spacing: "8px",
+    starWidth: "11px",
+    normalFill: "#d6d6d6",
+    ratedFill: "#ffcc00"
+  });
+  $(".product-one__rate").rateYo({
+    readOnly: true,
+    spacing: "13px",
+    starWidth: "18px",
+    normalFill: "#d6d6d6",
+    ratedFill: "#ffcc00"
+  });
+
+  $(".catalog-list__rate").rateYo({
+    readOnly: true,
+    spacing: "13px",
+    starWidth: "18px",
+    normalFill: "#d6d6d6",
+    ratedFill: "#ffcc00"
+  });
+
+  $(".comments__rate").rateYo({
+    readOnly: true,
+    spacing: "1px",
+    starWidth: "17px",
+    normalFill: "#d6d6d6",
+    ratedFill: "#ffcc00"
+  });
+  $(".review__rate").rateYo({
+    readOnly: false,
+    spacing: "1px",
+    starWidth: "17px",
+    normalFill: "#d6d6d6",
+    ratedFill: "#ffcc00"
+  });
+
   $('.slider-top__inner').slick({
     arrows: false,
     dots: true,
@@ -7,6 +84,34 @@ $(function () {
     autoplay: true,
     autoplaySpeed: 2000
   });
-  var mixer = mixitup('.week-catalog__content');
+  $('.product-slide__big').slick({
+    asNavFor: '.product-slide__thumb',
+    draggable: false,
+    arrows: false,
+    fade: true
+  });
+  $('.product-slide__thumb').slick({
+    asNavFor: '.product-slide__big',
+    focusOnSelect: true,
+    slidesToScroll: 1,
+    slidesToShow: 3,
+    vertical: true,
+    draggable: false
+  });
+  var mixer = mixitup('.catalog-grid', {
+    selectors: {
+      control: '.week-catalog__btn'
+    }
+  });
+  var mixer2 = mixitup('.design-catalog__content', {
+    selectors: {
+      control: '.design-catalog__btn'
+    }
+  });
+
+
+
 
 });
+
+
